28b12cdce57b5b07bef485a63f84625dbc201a8f
Pichi0927 <pichifree7@gmail.com>
2025-07-21 22:01:57 -0400

    Agregando los controles

---
+++ SistemaAgencia/Migrations/20250722015238_Inicial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaAgencia.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paises",
+                columns: table => new
+                {
+                    PaisID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paises", x => x.PaisID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Destino",
+                columns: table => new
+                {
+                    DestinoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaisID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destino", x => x.DestinoID);
+                    table.ForeignKey(
+                        name: "FK_Destino_Paises_PaisID",
+                        column: x => x.PaisID,
+                        principalTable: "Paises",
+                        principalColumn: "PaisID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Destino_PaisID",
+                table: "Destino",
+                column: "PaisID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destino");
+
+            migrationBuilder.DropTable(
+                name: "Paises");
+        }
+    }
+}
